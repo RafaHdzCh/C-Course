efb865745c1bd66ce9cfb61244b3d7e7162ddfba
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2022-09-09 13:00:09 -0500

    Initial commit

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Course
+{
+    public class Example : MonoBehaviour
+    {
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSharpCourse
+ Contenido AVANZADO como: C#, Visual Studio, POO, Patrones de Diseño, SOLID, Custom Editor, Git, GitHub, Errores
